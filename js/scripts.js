687222c905aabdfa8865abcdc780e5cc143b2cae
Dominic Montelongo <montelongodominic@gmail.com>
2020-03-24 09:50:17 -0700

    add functions to buttons in index.html using  scripts.js
    coauthored by Mark Gridley <mark_gridley@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
   $("#1").click(function() {
     $("img#taco1").slideToggle();
   });
+  $("button#green").click(function() {
+    $("body").removeClass ();
+    $("body").addClass("green-background");
+  });
+  $("button#yellow").click(function() {
+    $("body").removeClass ();
+    $("body").addClass("yellow-background");
+  });
+  $("button#red").click(function() {
+    $("body").removeClass ();
+    $("body").addClass("red-background");
+  });
   $("#2").click(function() {
     $("img#taco2").fadeToggle();
   });
